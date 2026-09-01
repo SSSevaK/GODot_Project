d68b918a76bffafdf83ac7e6df2ac9c88d30ecce
Kovensky_Pc <wispvsolo@gmail.com>
2026-09-01 19:54:49 +0300

    Реализована продажа зелей и получение за них денег. Колба пока не исчезает

--- Sevak_Assets/Колба.cs
+++ Sevak_Assets/Колба.cs
@@ -4,10 +4,16 @@ using System;
 public partial class Колба : Sprite2D
 {
     bool input = false;
+    bool check = false;
+
+    [Signal]
+    public delegate void ItemInZoneEventHandler(int id);
+
     public override void _Ready()
     {
-
+        
     }
+
 
     public override void _Process(double delta)
     {
@@ -15,6 +21,22 @@ public partial class Колба : Sprite2D
         {
             GlobalPosition = GetGlobalMousePosition();
         }
+        Rect2 zone = new Rect2(361, 510, 25, 25);
+        if (zone.HasPoint(GlobalPosition) && !check)
+        {
+            GD.Print("Poizon in area");
+            EmitSignal(SignalName.ItemInZone, 1);
+            check = true;
+        }
+        if (!zone.HasPoint(GlobalPosition) && check)
+        { 
+            check = false;
+        }
+
+
+
+
+
     }
     public override void _Input(InputEvent @event)
     {
---
+++ TextEdit.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class TextEdit : Godot.TextEdit
+{
+    // blue is 1
+    int Gold = 0;
+    public override void _Ready()
+	{
+        GetNode<Колба>("../flask/Колба").ItemInZone += CheckItem;
+        Text = Gold.ToString();
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+    private void CheckItem(int id)
+    {
+        if (id == 1)
+        {
+            GD.Print("Получено 100");
+            Gold += 100;
+            Text = Gold.ToString();
+        }
+    }
+}

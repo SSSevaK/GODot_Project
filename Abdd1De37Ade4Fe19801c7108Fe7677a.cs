4ce2ed8d293668298c7d68ea68eb67baec9d1b57
Kovensky_Pc <wispvsolo@gmail.com>
2026-09-06 21:34:28 +0300

    Создана работала лавки с нормальными/2 ассетами, ревлизована продажа, реализовано перемещение человека

---
+++ Abdd1De37Ade4Fe19801c7108Fe7677a.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public partial class Abdd1De37Ade4Fe19801c7108Fe7677a : Sprite2D
+{
+
+    public override void _Ready()
+    {
+    }
+
+    public override void _Process(double delta)
+    {
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouse)
+        {
+            if (mouse.Pressed)
+            {
+                if (GetGlobalMousePosition().DistanceTo(GlobalPosition) > 30)
+                {
+                    return;
+                }
+                if (mouse.ButtonIndex == MouseButton.Left)
+                {
+                    GetTree().CurrentScene.GetNode<TextEdit>("TextEdit").ForSell(1);
+                    QueueFree();
+                }
+
+                if (mouse.ButtonIndex == MouseButton.Right)
+                {
+                    GetTree().CurrentScene.GetNode<TextEdit>("TextEdit").ForSell(0);
+                }
+            }
+        }
+    }
+}
--- TextEdit.cs
+++ TextEdit.cs
@@ -3,26 +3,35 @@ using System;
 
 public partial class TextEdit : Godot.TextEdit
 {
-    // blue is 1
     int Gold = 0;
+    private HealingPotion potionOnTable;
     public override void _Ready()
-	{
-        GetNode<Колба>("../flask/Колба").ItemInZone += CheckItem;
+    {
         Text = Gold.ToString();
-
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-	{
-	}
-    private void CheckItem(int id)
+    public void ForSell (int id)
     {
         if (id == 1)
         {
             GD.Print("Получено 100");
             Gold += 100;
             Text = Gold.ToString();
+            if (potionOnTable != null)
+            {
+                potionOnTable.QueueFree();
+                potionOnTable = null;
+            }
         }
+
+        if (id == 0)
+        {
+            GD.Print("Торговля");
+        }
+
+    }
+    public void SetPotion(HealingPotion potion)
+    {
+        potionOnTable = potion;
     }
 }
